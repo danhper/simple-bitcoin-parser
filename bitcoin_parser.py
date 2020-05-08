43ef2d6c6c2f6f40ae78bf4f46a7a8100306efc5
Daniel Perez <daniel@perez.sh>
2020-05-08 14:51:23 +0200

    Add formatters for transaction

--- bitcoin_parser.py
+++ bitcoin_parser.py
@@ -1,5 +1,6 @@
 from typing import List
 import math
+import hashlib
 
 
 def parse_cuint(cuint: bytes) -> (int, int):
@@ -67,11 +68,14 @@ def format_nbits(value: int) -> bytes:
     return mantissa.to_bytes(3, "little") + exponent.to_bytes(1, "little")
 
 
-def parse_vector(raw_vector: bytes, parse_element: callable) -> (list, int):
+def parse_vector(raw_vector: bytes, parse_element: callable = None) -> (list, int):
     """Given a parsing function, parses a vector
+    Defaults to parsing elements as simple `uint8`
     >>> parse_vector(bytes([0x4, 0x0, 0x1, 0x2, 0x3]), lambda v: (v[0], 1))
     ([0, 1, 2, 3], 5)
     """
+    if parse_element is None:
+        parse_element = lambda raw_bytes: (raw_bytes[0], 1)
     element_count, offset = parse_cuint(raw_vector)
     results = []
     for _ in range(element_count):
@@ -81,11 +85,14 @@ def parse_vector(raw_vector: bytes, parse_element: callable) -> (list, int):
     return results, offset
 
 
-def format_vector(vector: list, format_element: callable) -> bytes:
+def format_vector(vector: list, format_element: callable = None) -> bytes:
     """Given a formatting function, formats a vector
+    Defaults to formatting elements as simple `uint8`
     >>> format_vector([0, 1, 2, 3], lambda v: v.to_bytes(1, 'little'))
     b'\\x04\\x00\\x01\\x02\\x03'
     """
+    if format_element is None:
+        format_element = lambda elem: elem.to_bytes(1, 'little')
     result = format_cuint(len(vector))
     for element in vector:
         result += format_element(element)
@@ -150,7 +157,8 @@ def parse_transaction_input(raw_tx_input: bytes) -> (dict, int):
     ... "a94e0aad311a4268e082a725f8aeae05"
     ... "73fb12ff866a5f01"                   # Secp256k1 signature
     ... "ffffffff")
-    >>> tx_input, _consumed = parse_transaction_input(raw_tx_input)
+    >>> tx_input, consumed = parse_transaction_input(raw_tx_input)
+    >>> assert(consumed == len(raw_tx_input))
     >>> assert(tx_input["previous_index"] == 0)
     >>> assert(len(tx_input["script"]) == 73)
     >>> assert(tx_input["sequence"] == 0xffffffff)
@@ -158,10 +166,8 @@ def parse_transaction_input(raw_tx_input: bytes) -> (dict, int):
     """
     previous_hash = raw_tx_input[0:32]
     previous_index = int.from_bytes(raw_tx_input[32:36], "little")
-    script_size, consumed = parse_cuint(raw_tx_input[36:])
+    script, consumed = parse_vector(raw_tx_input[36:])
     index = consumed + 36
-    script = raw_tx_input[index:index + script_size]
-    index += script_size
     sequence = int.from_bytes(raw_tx_input[index:index + 4], "little")
     return {
         "previous_hash": previous_hash,
@@ -169,6 +175,22 @@ def parse_transaction_input(raw_tx_input: bytes) -> (dict, int):
         "script": script,
         "sequence": sequence,
     }, index + 4
+
+
+def format_transaction_input(tx_input: dict) -> bytes:
+    """Formats a transaction input into its serialized representation
+    >>> raw_tx_input = bytes.fromhex(
+    ... "7b1eabe0209b1fe794124575ef807057c77ada2138ae4fa8d6c4de0398a14f3f"
+    ... "00000000494830450221008949f0cb400094ad2b5eb399d59d01c14d73d8fe6e96df1a7150de"
+    ... "b388ab8935022079656090d7f6bac4c9a94e0aad311a4268e082a725f8aeae0573fb12ff866a5f01ffffffff")
+    >>> tx_input, _consumed = parse_transaction_input(raw_tx_input)
+    >>> assert(format_transaction_input(tx_input) == raw_tx_input)
+    """
+    result = tx_input["previous_hash"]
+    result += tx_input["previous_index"].to_bytes(4, "little")
+    result += format_vector(tx_input["script"])
+    result += tx_input["sequence"].to_bytes(4, "little")
+    return result
 
 
 def parse_transaction_output(raw_tx_output: bytes) -> (dict, int):
@@ -183,32 +205,46 @@ def parse_transaction_output(raw_tx_output: bytes) -> (dict, int):
     ...   "e7c6c772"                           # PubKey hash
     ...   "88"                                 # OP_EQUALVERIFY
     ...   "ac")                                # OP_CHECKSIG
-    >>> tx_output, _consumed = parse_transaction_output(raw_tx_output)
+    >>> tx_output, consumed = parse_transaction_output(raw_tx_output)
+    >>> assert(consumed == len(raw_tx_output))
     >>> assert(tx_output["value"] == 4999990000)
     >>> assert(len(tx_output["script"]) == 25)
     """
-    value = int.from_bytes(raw_tx_output[0:8], "little", signed=False)
-    script_size, consumed = parse_cuint(raw_tx_output[8:])
-    index = 8 + consumed
-    script = raw_tx_output[index:index + script_size]
+    value = int.from_bytes(raw_tx_output[0:8], "little", signed=True)
+    script, consumed = parse_vector(raw_tx_output[8:])
     return {
         "value": value,
         "script": script,
-    }, index + script_size
+    }, 8 + consumed
 
 
-
-def parse_witness(raw_witness: bytes) -> (bytes, int):
-    """Parse a transaction witnesses
+def format_transaction_output(tx_output: dict) -> bytes:
+    """Formats a transaction output in its bytes representation
+    >>> raw_tx_output = bytes.fromhex(
+    ...   "f0ca052a010000001976a914"
+    ...   "cbc20a7664f2f69e5355aa427045bc15e7c6c77288ac")
+    >>> tx_output, _consumed = parse_transaction_output(raw_tx_output)
+    >>> assert(format_transaction_output(tx_output) == raw_tx_output)
     """
-    size, consumed = parse_cuint(raw_witness)
-    return raw_witness[0:size], consumed + size
+    return tx_output["value"].to_bytes(8, "little", signed=True) + \
+           format_vector(tx_output["script"])
 
 
 def parse_transaction(raw_transaction: bytes) -> (dict, int):
-    """Parse a raw transaction
-    >>> raw_tx = bytes.fromhex("0200000000010140d43a99926d43eb0e619bf0b3d83b4a31f60c176beecfb9d35bf45e54d0f7420100000017160014a4b4ca48de0b3fffc15404a1acdc8dbaae226955ffffffff0100e1f5050000000017a9144a1154d50b03292b3024370901711946cb7cccc387024830450221008604ef8f6d8afa892dee0f31259b6ce02dd70c545cfcfed8148179971876c54a022076d771d6e91bed212783c9b06e0de600fab2d518fad6f15a2b191d7fbd262a3e0121039d25ab79f41f75ceaf882411fd41fa670a4c672c23ffaf0e361a969cde0692e800000000")
-    >>> tx = parse_transaction(raw_tx)
+    """Parses a raw transaction
+    >>> raw_tx = bytes.fromhex("0200000000010140d43a99926d43eb0e619bf0b3"
+    ...                        "d83b4a31f60c176beecfb9d35bf45e54d0f74201"
+    ...                        "00000017160014a4b4ca48de0b3fffc15404a1ac"
+    ...                        "dc8dbaae226955ffffffff0100e1f50500000000"
+    ...                        "17a9144a1154d50b03292b3024370901711946cb"
+    ...                        "7cccc387024830450221008604ef8f6d8afa892d"
+    ...                        "ee0f31259b6ce02dd70c545cfcfed81481799718"
+    ...                        "76c54a022076d771d6e91bed212783c9b06e0de6"
+    ...                        "00fab2d518fad6f15a2b191d7fbd262a3e012103"
+    ...                        "9d25ab79f41f75ceaf882411fd41fa670a4c672c"
+    ...                        "23ffaf0e361a969cde0692e800000000")
+    >>> tx, consumed = parse_transaction(raw_tx)
+    >>> assert(consumed == len(raw_tx))
     >>> assert(tx["version"] == 2)
     >>> assert(len(tx["inputs"]) == 1)
     >>> assert(len(tx["inputs"][0]["witnesses"]) == 2)
@@ -233,7 +269,7 @@ def parse_transaction(raw_transaction: bytes) -> (dict, int):
     index += consumed
     if has_witness:
         for tx_input in inputs:
-            witnesses, consumed = parse_vector(raw_transaction[index:], parse_witness)
+            witnesses, consumed = parse_vector(raw_transaction[index:], parse_vector)
             tx_input["witnesses"] = witnesses
             index += consumed
     locktime = int.from_bytes(raw_transaction[index:index + 4], "little")
@@ -242,4 +278,46 @@ def parse_transaction(raw_transaction: bytes) -> (dict, int):
         inputs=inputs,
         outputs=outputs,
         locktime=locktime,
-    )
+    ), index + 4
+
+
+def double_sha256(raw_bytes: bytes) -> bytes:
+    """Computes a double SHA256 hash
+    """
+    return hashlib.sha256(hashlib.sha256(raw_bytes).digest()).digest()
+
+
+def format_transaction(transaction: dict, with_witness: bool = True) -> bytes:
+    """Formats a transaction in its bytes representation
+    >>> raw_tx = bytes.fromhex("0200000000010140d43a99926d43eb0e619bf0b3"
+    ...                        "d83b4a31f60c176beecfb9d35bf45e54d0f74201"
+    ...                        "00000017160014a4b4ca48de0b3fffc15404a1ac"
+    ...                        "dc8dbaae226955ffffffff0100e1f50500000000"
+    ...                        "17a9144a1154d50b03292b3024370901711946cb"
+    ...                        "7cccc387024830450221008604ef8f6d8afa892d"
+    ...                        "ee0f31259b6ce02dd70c545cfcfed81481799718"
+    ...                        "76c54a022076d771d6e91bed212783c9b06e0de6"
+    ...                        "00fab2d518fad6f15a2b191d7fbd262a3e012103"
+    ...                        "9d25ab79f41f75ceaf882411fd41fa670a4c672c"
+    ...                        "23ffaf0e361a969cde0692e800000000")
+    >>> tx, _consumed = parse_transaction(raw_tx)
+    >>> assert(format_transaction(tx) == raw_tx)
+    >>> expected_txid = bytes.fromhex("c586389e5e4b3acb9d6c8be1c19ae8ab2795397633176f5a6442a261bbdefc3a")[::-1]
+    >>> expected_hash = bytes.fromhex("b759d39a8596b70b3a46700b83e1edb247e17ba58df305421864fe7a9ac142ea")[::-1]
+    >>> assert(double_sha256(raw_tx) == expected_hash)
+    >>> assert(double_sha256(format_transaction(tx, with_witness=True)) == expected_hash)
+    >>> assert(double_sha256(format_transaction(tx, with_witness=False)) == expected_txid)
+    """
+    has_witness = any("witnesses" in tx_in for tx_in in transaction["inputs"])
+    include_witness = with_witness and has_witness
+
+    result = transaction["version"].to_bytes(4, "little", signed=True)
+    if include_witness:
+        result += bytes([0, 1])  # marker and flags
+    result += format_vector(transaction["inputs"], format_transaction_input)
+    result += format_vector(transaction["outputs"], format_transaction_output)
+    if include_witness:
+        for tx_input in transaction["inputs"]:
+            result += format_vector(tx_input.get("witnesses", []), format_vector)
+    result += transaction["locktime"].to_bytes(4, "little")
+    return result
